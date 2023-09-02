283db66a547633d40a1c163a59733aadd335ca03
Aether-127 <143834764+Aether-127@users.noreply.github.com>
2023-09-02 12:03:22 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -13,3 +13,21 @@ echo "User output: "
 cat user_output1.txt 
 echo "Expected output: " 
 cat output1.txt 
+
+
+#Task 2
+for var in "$@"
+do 
+	echo "Running $var on test cases"
+	for i in $(seq 3)
+	do
+		echo "Running test case $i:"
+		cat input$i.txt | python3 $var > user_output$i.txt 
+		echo "User output: " 
+		cat user_output$i.txt 
+		echo "Expected output: " 
+		cat output$i.txt 
+		echo "--------------------------------------------------------------"
+	done
+	echo "================================================================="
+done
